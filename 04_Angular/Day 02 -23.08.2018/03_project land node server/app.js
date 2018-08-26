7ee862323742bc99a3582bc431f87e3d30504947
AnnaKarpf <32166631+AnnaKarpf@users.noreply.github.com>
2018-08-26 10:35:05 +0300

    4578

--- 04_Angular/Day 02 -23.08.2018/03_project land node server/app.js
+++ 04_Angular/Day 02 -23.08.2018/03_project land node server/app.js
@@ -5,6 +5,20 @@ const express = require('express');
 const app = express();
 const basePath = path.join(__dirname + "/dist");
 
+
+app.get(`/`, (req, res) => {
+    let linkList = "";
+    let resPage=fs.readFileSync("links.html","utf-8");
+   console.log(resPage);
+    fs.readdir(basePath, (err, files) => {
+        files.forEach((file) => {
+            linkList += `<li><a href="/${file}" target="blank">${file}</a></li>`;
+        })
+        res.send(resPage.replace("placeHolder", linkList));
+    });
+
+});
+
 fs.readdir(basePath, (err, files) => {
     files.forEach((file) => {
         app.use(express.static(`${basePath}/${file}`));
@@ -15,4 +29,4 @@ fs.readdir(basePath, (err, files) => {
 });
 
 const port = process.env.PORT || 3500;
-app.listen(port,  ()=> { console.log(`OK`); });
+app.listen(port, () => { console.log(`OK`); });
