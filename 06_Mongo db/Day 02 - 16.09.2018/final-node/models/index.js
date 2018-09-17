b69d2ad3976f2976dc598ed5c49ddf993246b617
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-09-17 12:33:05 +0300

    4578

--- 06_Mongo db/Day 02 - 16.09.2018/final-node/models/index.js
+++ 06_Mongo db/Day 02 - 16.09.2018/final-node/models/index.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 const { seedMongoDb } = require('./seeders/seeder');
 
 // Connect to MongoDB: 
-var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb://test:test@cluster0-shard-00-00-e0hcb.mongodb.net:27017,cluster0-shard-00-01-e0hcb.mongodb.net:27017,cluster0-shard-00-02-e0hcb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+let uri = "mongodb://test:test@cluster0-shard-00-00-e0hcb.mongodb.net:27017,cluster0-shard-00-01-e0hcb.mongodb.net:27017,cluster0-shard-00-02-e0hcb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 mongoose.connect(uri, { useNewUrlParser: true })
     .then(() => {
