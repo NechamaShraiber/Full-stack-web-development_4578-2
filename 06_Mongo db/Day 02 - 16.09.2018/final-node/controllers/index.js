f7bf17edec200824709df158f7196f6f917c656b
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-09-17 10:38:58 +0300

    4578

--- 06_Mongo db/Day 02 - 16.09.2018/final-node/controllers/index.js
+++ 06_Mongo db/Day 02 - 16.09.2018/final-node/controllers/index.js
@@ -1,12 +1,17 @@
 // Requires:
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
+const path = require('path');
+const fs = require('fs');
 
 const bookController=require('./book');
 const countryController=require('./country');
 const clientController=require('./client');
 
 const mongoose = require("./../models/index");
+
+
+const basePath = path.join(__dirname + "/../views");
 
 // Create express app:
 const app = express();
@@ -16,11 +21,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Use middlewares - enable all the files in the folder:
-app.use(express.static("C:\\Users\\User\\Desktop\\final-node\\views"));   
+app.use(express.static(basePath));   
 
 // return to every "bookStore" req a html page
 app.get("/bookStore",(req,res)=>{  
-    res.sendFile("C:\\Users\\User\\Desktop\\final-node\\views\\index.html");
+    res.sendFile(basePath+"/index.html");
 });
 
 bookController.init(app);
