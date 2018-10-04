bd3ea8bfa08ebd62f9adef22721e0d959fdab1f8
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-10-04 08:58:27 +0300

    4578

---
+++ 08_Basic c#/Day 03 - 04.10.2018/05 - Send Object to Task/Send Object to Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Send_Object_to_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            Range range = new Range(start, end);
+
+            Task t = new Task(obj => {
+
+                Range r = obj as Range;
+
+                for (int i = r.Start; i <= r.End; i++)
+                {
+                    Console.Write(i + " ");
+                    Thread.Sleep(200);
+                }
+            }, range);
+
+            t.Start();
+            t.Wait();
+        }
+    }
+}
---
+++ 08_Basic c#/Day 03 - 04.10.2018/10 - Task Exceptions Exercise/Task Exceptions Exercise/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_Exceptions_Exercise
+{
+    class Program
+    {
+        static object lockFlag = new object();
+
+        static void Main(string[] args)
+        {
+            Task parent = new Task(()=> {
+
+                Task t1 = new Task(()=> {
+                    int a = 0, b = 0;
+                    lock (lockFlag)
+                    {
+                        Console.Write("Enter first number: ");
+                        a = int.Parse(Console.ReadLine());
+                        Console.Write("Enter second number: ");
+                        b = int.Parse(Console.ReadLine());
+                    }
+                    Thread.Sleep(3000);
+                    Console.WriteLine(a / b);
+                }, TaskCreationOptions.AttachedToParent);
+
+                Task t2 = new Task(() => {
+                    int[] array = new int[5];
+                    for (int i = 0; i < array.Length; i++)
+                        array[i] = i * i;
+                    Thread.Sleep(10000);
+                    Console.WriteLine(array[7]);
+                }, TaskCreationOptions.AttachedToParent);
+
+                Task t3 = new Task(() => {
+                    string file = "";
+                    lock (lockFlag)
+                    {
+                        Console.Write("Enter file name: ");
+                        file = Console.ReadLine();
+                    }
+                    StreamReader reader = new StreamReader(file);
+                }, TaskCreationOptions.AttachedToParent);
+
+                t1.Start();
+                t2.Start();
+                t3.Start();
+            });
+
+            parent.Start();
+
+            try
+            {
+                parent.Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception item in ex.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine(item.Message);
+                    Console.WriteLine(item.GetType().Name);
+                    Console.WriteLine("---------------------------");
+                }
+            }
+        }
+    }
+}
